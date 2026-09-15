e4cd61643e12abcfbcc76105a5d962639d2a24ed
scottbrys <sbrys@ssrtotalit.com>
2026-09-15 13:41:00 -0500

    Update ScreenConnect-Upgrade-v8.ps1

--- ScreenConnect-Upgrade-v8.ps1
+++ ScreenConnect-Upgrade-v8.ps1
@@ -13,7 +13,7 @@ Write-Host "[$timestamp] === ScreenConnect Upgrade v8 Started ===" -ForegroundCo
 # Enforce TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$msiUrl = "https://raw.githubusercontent.com/scottbrys/Public/releases/download/v26.6.5.9742/ScreenConnect-Upgrade-Clean.msi"
+$msiUrl = "https://raw.githubusercontent.com/scottbrys/Public/main/ScreenConnect-Upgrade-Clean.msi"
 $msiPath = "C:\temp\ScreenConnect-$TargetVersion.msi"
 
 # Registry paths to search
